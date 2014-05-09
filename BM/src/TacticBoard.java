87a9b6d3f0bf96cb2a53a4338789011fec824f7b
Onur Barış DEV <onurbarisdev@hotmail.com>
2014-05-09 22:30:29 +0300

    TacticBoard

--- BM/src/TacticBoard.java
+++ BM/src/TacticBoard.java
@@ -1,6 +1,22 @@
 
 
 public class TacticBoard {
+	Player[] inGamePlayers=new Player[5];
 
+	public TacticBoard(Player[] inGamePlayers) {
+		super();
+		this.inGamePlayers = inGamePlayers;
+	}
+
+	
+	public Player[] getInGamePlayers() {
+		return inGamePlayers;
+	}
+
+
+	public void setInGamePlayers(Player[] inGamePlayers) {
+		this.inGamePlayers = inGamePlayers;
+	}
+	
 	
 }
