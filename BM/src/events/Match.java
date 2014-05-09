9f921629bed88e3c5118aa2f20d7d08e914aff3c
ayleyid <ayleyid@gmail.com>
2014-05-09 23:00:59 +0300

    update match class

--- BM/src/events/Match.java
+++ BM/src/events/Match.java
@@ -27,5 +27,23 @@ public class Match implements Event {
 		
 		return matchTime;
 	}
+	public Team getHomeTeam() {
+		return homeTeam;
+	}
+	public void setHomeTeam(Team homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+	public Team getAwayTeam() {
+		return awayTeam;
+	}
+	public void setAwayTeam(Team awayTeam) {
+		this.awayTeam = awayTeam;
+	}
+	public ScoreBoard getScoreBoard() {
+		return scoreBoard;
+	}
+	public void setScoreBoard(ScoreBoard scoreBoard) {
+		this.scoreBoard = scoreBoard;
+	}
 
 }
