efb6e890ad69d9d0e49a5a8b4f7f97c044ed13f2
ayleyid <ayleyid@gmail.com>
2014-05-11 23:18:06 +0300

    syntax check

--- BM/src/gameengine/Game.java
+++ BM/src/gameengine/Game.java
@@ -1,19 +1,21 @@
 package gameengine;
+
 import teams.League;
 import events.Event;
 
-
 public class Game {
-Time currentTime;
-League league;
-public Game(Time currentTime, League league) {
-	this.currentTime = currentTime;
-	this.league = league;
-}
-public void advanceInTime(){
-	currentTime.nextDay();
-	Event event=league.getTodaysEvent(currentTime);
-	event.perform();
-}
+	Time currentTime;
+	League league;
+
+	public Game(Time currentTime, League league) {
+		this.currentTime = currentTime;
+		this.league = league;
+	}
+
+	public void advanceInTime() {
+		currentTime.nextDay();
+		Event event = league.getTodaysEvent(currentTime);
+		event.perform();
+	}
 
 }
