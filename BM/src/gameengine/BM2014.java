3302e28e095ba1c8feb317655faf6275f9ba2a9d
ayleyid <ayleyid@gmail.com>
2014-05-13 21:09:36 +0300

    updated main class

--- BM/src/gameengine/BM2014.java
+++ BM/src/gameengine/BM2014.java
@@ -1,13 +1,25 @@
 package gameengine;
+
+import java.util.Scanner;
+
 public class BM2014 {
 
 	public static void main(String[] args) {
 		new BM2014();
 	}
-	private BM2014(){
+
+	private BM2014() {
 		startApp();
 	}
-	void startApp(){
-		Game game = new Game(new Time(1,1,2014));
+
+	void startApp() {
+		Game game = new Game(new Time(1, 1, 2014));
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String str = sc.nextLine();
+			if (str.equals("exit"))
+				break;
+			game.advanceInTime();
+		}
 	}
 }
