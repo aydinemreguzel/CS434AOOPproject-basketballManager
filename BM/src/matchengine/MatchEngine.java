a104f0b77af586b4a73b9c243f174e44f4463ea2
ayleyid <ayleyid@gmail.com>
2014-05-13 23:24:30 +0300

    machengine update

--- BM/src/matchengine/MatchEngine.java
+++ BM/src/matchengine/MatchEngine.java
@@ -1,8 +1,8 @@
 package matchengine;
 
-import java.util.Random;
-
 import teams.Player;
+import teams.ScoreBoard;
+import teams.TacticBoard;
 import events.MatchEvent;
 
 // due to incomplete tactics board class lots of magic number in match engine and state classes
@@ -12,16 +12,18 @@ public class MatchEngine {
 	int attackOrder;
 	int matchClock = 2400; // 40 min = 2400 sec
 	int shotClock = 24; // sec
-	int[] moral = { 10, 10 };
 	int positioning;
-	int[] score = { 0, 0 };
-	Player[] InGameHome = new Player[5];
-	Player[] InGameAway = new Player[5];
+	ScoreBoard homeSB;
+	ScoreBoard awaySB;
+	TacticBoard homeTB;
+	TacticBoard awayTB;
 
 	public MatchEngine(MatchEvent match) {
 		state = new AdvancingBallState();
-		InGameHome = match.getHomeTeam().getTacticBoard().getInGamePlayers();
-		InGameAway = match.getAwayTeam().getTacticBoard().getInGamePlayers();
+		homeTB = match.getHomeTacticBoard();
+		awayTB = match.getAwayTacticBoard();
+		homeSB = match.getHomeScoreBoard();
+		awaySB = match.getAwayScoreBoard();
 	}
 
 	public void play() {
@@ -46,16 +48,16 @@ public class MatchEngine {
 
 	public Player getBallHandlerPlayer() {
 		if (attackOrder == 0)
-			return InGameHome[ballHandler];
+			return homeTB.getInGamePlayers()[ballHandler];
 		else
-			return InGameAway[ballHandler];
+			return awayTB.getInGamePlayers()[ballHandler];
 	}
 
 	public Player getBallDefenderPlayer() {
 		if (attackOrder == 1)
-			return InGameHome[ballHandler];
+			return homeTB.getInGamePlayers()[ballHandler];
 		else
-			return InGameAway[ballHandler];
+			return awayTB.getInGamePlayers()[ballHandler];
 	}
 
 	public void setState(State state) {
@@ -102,14 +104,6 @@ public class MatchEngine {
 		this.shotClock = 24;
 	}
 
-	public int[] getMoral() {
-		return moral;
-	}
-
-	public void setMoral(int[] moral) {
-		this.moral = moral;
-	}
-
 	public int getPositioning() {
 		return positioning;
 	}
@@ -118,12 +112,11 @@ public class MatchEngine {
 		this.positioning = positioning;
 	}
 
-	public int[] getScore() {
-		return score;
-	}
-
-	public void increaseScore(int score) {
-		this.score[getAttackOrder()] += score;
+	public void increaseScore(int score,int player) {
+		if(getAttackOrder() == 0)
+			homeSB.increaseScore(score,player);
+		if(getAttackOrder() == 1)
+			awaySB.increaseScore(score,player);
 	}
 
 	public void increasePositioning(int i) {
