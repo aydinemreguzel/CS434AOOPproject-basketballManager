8276100a44c3f3aa431df1d36d3532a0687d274f
ayleyid <ayleyid@gmail.com>
2014-05-13 21:05:22 +0300

    Game chanced

--- BM/src/gameengine/Game.java
+++ BM/src/gameengine/Game.java
@@ -6,10 +6,10 @@ import events.Event;
 public class Game {
 	Time currentTime;
 	League league;
-	
-	public Game(Time currentTime){
+
+	public Game(Time currentTime) {
 		this.currentTime = currentTime;
-		generateLeague("League1",4);
+		generateLeague("League1", 4);
 	}
 
 	public Game(Time currentTime, League league) {
@@ -22,9 +22,12 @@ public class Game {
 		Event event = league.getTodaysEvent(currentTime);
 		event.perform();
 	}
-	
-	public void generateLeague(String leagueName,int numTeams){
-		league = new League(leagueName,numTeams,currentTime.moveOnTime(15, 0, 0));
+
+	public void generateLeague(String leagueName, int numTeams) {
+		Time leagueTime = new Time(currentTime.getDay(),
+				currentTime.getMonth(), currentTime.getYear());
+		leagueTime.moveOnTime(15, 0, 0);
+		league = new League(leagueName, numTeams, leagueTime);
 	}
 
 }
