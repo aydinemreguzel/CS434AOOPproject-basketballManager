cef725167058733f23c0e850762e0dc539bc2076
Onur Barış DEV <onurbarisdev@hotmail.com>
2014-05-15 18:19:59 +0300

    asd

--- BM/src/teams/ScoreBoard.java
+++ BM/src/teams/ScoreBoard.java
@@ -3,20 +3,24 @@ import java.util.Arrays;
 
 public class ScoreBoard {
 	// STATS
-	private static final int MIN   = 0;
-	private static final int PM2_A = 1;
-	private static final int PM3_A = 2;
-	private static final int FTM_A = 3;
-	private static final int RB    = 4;
-	private static final int PF    = 5;
-	private static final int ST    = 6;
-	private static final int TO    = 7;
-	private static final int BS    = 8;
-	private static final int BA    = 9;
-	private static final int PTS   = 10;
+	private static final int MIN   = 0;   //minute in game
+	private static final int PM2_A = 1;   //2point attempt
+	private static final int PM2_S = 2;   //2point success
+	private static final int PM3_A = 3;   //3point attempt
+	private static final int PM3_S = 4;	  //3point success	
+	private static final int FTM_A = 5;   //free throw attempt
+	private static final int FTM_S = 6;   //free throw success
+	private static final int RB    = 7;   //rebounds
+	private static final int PF    = 8;   //fouls
+	private static final int ST    = 9;   //steals
+	private static final int TO    = 10;  //turnover
+	private static final int BS    = 11;  //blocked shot
+	private static final int BA    = 12;  //block against
+	private static final int PTS   = 13;  //points
+	private static final int DIFF  = 14;  // difference
 
 	String[] names = new String[12];
-	int[][] stats = new int[12][12];
+	int[][] stats = new int[12][13];
 
 	@Override
 	public String toString() {//TODO
