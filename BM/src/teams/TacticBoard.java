a1b7c48646cf80a89eb758cfac2a0fb9a3a8d480
Onur Barış DEV <onurbarisdev@hotmail.com>
2014-06-05 22:46:34 +0300

    TacticBoard now points to team's players instead of keeping seperate arrays

--- BM/src/teams/TacticBoard.java
+++ BM/src/teams/TacticBoard.java
@@ -1,53 +1,55 @@
 package teams;
 
+import java.util.Collections;
 import java.util.List;
 
 public class TacticBoard {
-	Player[] onFloorPlayers;
-	Player[] inMatchPlayers;
+
 	int[] defenceOrder;
+	List<Player> players;
 
 	public TacticBoard(List<Player> players) {
 		super();
-		inMatchPlayers = new Player[12];
-		onFloorPlayers = new Player[5];
+		this.players = players;
+
 		defenceOrder = new int[5];
-		for (int i = 0; i < onFloorPlayers.length; i++) {
-			onFloorPlayers[i] = players.get(i);
-		}
-		for (int i = 0; i < inMatchPlayers.length; i++) {
-			inMatchPlayers[i] = players.get(i);
-		}
 		for (int i = 0; i < 5; i++) {
 			defenceOrder[i] = i;
 		}
 	}
 
 	public Player[] getOnFloorPlayers() {
+
+		Player[] onFloorPlayers = new Player[5];
+
+		for (int i = 0; i < onFloorPlayers.length; i++) {
+			onFloorPlayers[i] = players.get(i);
+		}
 		return onFloorPlayers;
 	}
-	
+
 	public Player[] getInMatchPlayers() {
+		Player[] inMatchPlayers = new Player[12];
+		;
+		for (int i = 0; i < inMatchPlayers.length; i++) {
+			inMatchPlayers[i] = players.get(i);
+		}
 		return inMatchPlayers;
 	}
 
-	public void setInGamePlayers(Player[] inGamePlayers) {
-		this.onFloorPlayers = inGamePlayers;
-	}
-	
-	public Player[] getInGamePlayers() {
-		return onFloorPlayers;
+	public void swapPlayers(int i, int j) {
+		Collections.swap(players, i, j);
 	}
 
 	public int getPlayerNum(int j) {
-		for(int i = 0;i<12;i++)
-			if(inMatchPlayers[j].equals(inMatchPlayers[i]))
+		for (int i = 0; i < 12; i++)
+			if (getOnFloorPlayers()[j].equals(getInMatchPlayers()[i]))
 				return i;
 		return -1;
 	}
 
 	public int getDefender(int i) {
 		return defenceOrder[i];
-	}	
+	}
 
 }
